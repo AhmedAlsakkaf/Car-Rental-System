da33eac523291bee7098c166daca4cfd128371c3
Ahmed abdulkareem <ahmedabdalkaremm@gmail.com>
2024-05-24 19:43:15 +0500

    HeaderResponsivnessDone

---
+++ js/home.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const navIcon = document.querySelector('.nav-icon');
+  const navMenu = document.querySelector('.nav');
+
+  navIcon.addEventListener('click', function() {
+    navMenu.classList.toggle('show');
+  });
+});
+
